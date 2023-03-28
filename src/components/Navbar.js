8f0e20a94d8aaea519a5c039fda8a4c02b581644
amber-02 <91199607+amber-02@users.noreply.github.com>
2023-03-28 20:37:36 +0100

    hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
-  return (
+    const {click, setClick} = useState(false)
+    // reverses what click is
+    const handleClick = () => setClick(!click)
+    return (
     <>
     <nav className="navbar">
+    <Link to="/" className="Navbar-logo">
+             RS ACADEMY
+             </Link>
         <div classname = "navbar-container">
             {/* putting the logo in the navbar - could also use 'a' tag */}
 
             <img className="logo" src="RS-logo.png" height={100}></img>
             
-            <div className="menu-icon">
-                {/* not clicked: hamburge3r menu */}
-                {/* i className = {click ? 'fas fa-times' : 'fas fa-bars'} */}
+            <div className="menu-icon" onClick={handleClick}>
+                {/* not clicked: hamburger menu */}
+                <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
         </div>
     </nav>
