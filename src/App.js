bc996fbb3284771b0569054ddb68212b0eddff23
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-03-28 22:51:20 +0100

    navbar and hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
 
 function App() {
   return (
