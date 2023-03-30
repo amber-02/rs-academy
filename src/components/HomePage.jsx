886f4afdb9e65cd469f859fa980012a3e0027d7c
amber-02 <91199607+amber-02@users.noreply.github.com>
2023-03-30 15:36:48 +0100

    amber changes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <>
       <div className='content'>
-          <div>
+          <div className = 'divstuff'> 
             <img className='rs-logo' src='logo-cap.png' alt="rs-logo"></img>
           </div>
           <p className='content-text'>Real Support Academy (also known as RS-Academy) is a community interest company striving to provide the best quality education and training to socially excluded community members with little or no access to formal education. One of its primary purposes is to build the skills and confidence of traditionally disadvantaged community members, including refugees and members of ethnic minority communities, to support their entry into meaningful employment. In addition, Real Support Academy strives to integrate learners into society’s social and cultural fabric and support their entry into meaningful work by providing broad and varied learning programmes to meet the changing needs of adults, refugees, families, communities and other stakeholders.</p>
