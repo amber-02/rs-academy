817313e3b2f1dcb7fe4bad3875667d2e00a0d651
ec21338 <ec21338@qmul.ac.uk>
2023-04-04 15:20:22 +0100

    backend added

---
+++ backend/src/components/Forum/ViewQuestions.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebase';
+import { onValue, ref, get } from 'firebase/database';
+import AddQuestion from 'AddQuestion';
+
+const ViewQuestions = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const questionsRef = ref(db, 'questions');
+    onValue(questionsRef, (snapshot) => {
+      const questionsArray = [];
+      snapshot.forEach((childSnapshot) => {
+        const question = childSnapshot.val().question;
+        const key = childSnapshot.key;
+        questionsArray.push({ key, question });
+      });
+      setQuestions(questionsArray);
+    });
+  }, []);
+
+  const [{answerQuestion}, setAnswerQuestion] = useState(false);
+  const showAnswerQuestion = () => {
+    setAnswerQuestion(true);
+  };
+
+  return (
+    <div className='view-questions-container'>
+      <h1>Forum of Questions</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Question</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map((question) => (
+            <tr key={question.key}>
+              <td>{question.question}</td>
+              <td>
+                {answerQuestion ? ( <AddQuestion /> ) : (
+                <Button text="Reply" onClick={showAnswerQuestion} />
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ViewQuestions;
