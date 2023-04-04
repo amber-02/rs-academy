8fe454b77d51602a333cc71bc48d2c06d2fe198a
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-04-04 20:38:29 +0100

    contact page - needs fixing responsiveness

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,20 +1,19 @@
+import './AboutUs.css';
+
 const AboutUs = () => {
   return (
     <>
-      <div className='content'>
+      <div className='content-aboutus'>
           {/* <div className = 'divstuff'> 
             <img className='rs-logo' src='logo-cap.png' alt="rs-logo"></img>
           </div> */}
-          <div className = 'divstuff'> 
-              <img className='rs-logo' src='graphic3.gif' alt="graphic 3"></img>
+          <div className='head-items'>
+            <img className='graphic' src='graphic3.gif' alt="graphic 3"></img>
+            <div className='title-text'>
+              <h1 className ='title-aboutus'>About Us</h1>
+              <p className='description'>short paragraph about us blablablac dkjvchbekcj  c fkjfb cjs cejkbfvkjbkjcw cbekjb cwk</p>
             </div>
-          <h1 className = 'title'>
-              about us
-            </h1>
-            <p className='content-text'>About us</p>
-            <p className='content-text'>About us</p>
-            <p className='content-text'>About us</p>
-            <p className='content-text'>About us</p>
+          </div>
       </div>
     </>
   )
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,27 +1,24 @@
+import './ContactUs.css';
+
 const ContactUs = () => {
     return (
       <>
-        <div className='content'>
-            <div className = 'divstuff'> 
-              <img className='rs-logo' src='graphic1.gif' alt="graphic 1"></img>
+        <div className='content-contact'>
+              {/* <img className='rs-logo' src='Webinar-2.gif' alt="graphic 1"></img>
             </div>
-            {/* <div>
-            <img className='graphic' src='graphic1.gif' alt="graphic 1"></img>
-            </div> */}
-            <h1 className = 'title'>
-              Contact us
-            </h1>
-            <div className='content-text'>
-              <p>
-                call us: 123-456-7890
-              </p>
-              <p>
-                send us an email: realsupportacademy@rs.org
-              </p>
-              <p>
-                visit us: real support academy, 1234 road, anytown, 12345
-              </p>
+            <div> */}
+            <div className="head-items">
+              <div className='text'>
+                <h1 className ='title-contactus'>Contact us</h1>
+                <div className='content-text-contact'>
+                  <div className='line'><h1 className='contact-bold'>call us</h1><p className='contact-p'> 123-456-7890</p></div>
+                  <div className='line'><h1 className='contact-bold'>send us an email</h1><p className='contact-p'> realsupportacademy@rs.org</p></div>
+                  <div className='line'><h1 className='contact-bold'>visit us</h1><p className='contact-p'> real support academy, 1234 road, anytown, 12345</p></div>
+                </div>
+              </div>
+              <img className='graphic' src='graphic2.gif' alt="graphic 1"></img>
             </div>
+            
         </div>
         {/* <div>
           <p className='title'>Contact us</p>
