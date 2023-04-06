2104c09519b23c6046385b68feb519cab1e8b1f8
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-04-06 20:53:49 +0100

    design of the course page

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -4,60 +4,64 @@ const Courses = () => {
     return (
       <>
             <div className='content'>
-            <div className="course-title">
-                <h1 className ='title-courses'>Courses</h1>
-            </div>
+            
+            
+            <div className='all-courses-box'>
+                <div className="course-title">
+                    <h1 className ='title-courses'>All Courses</h1>
+                </div>
 
-            <div className="courserow1">
-            <div className="course-container">
-                <div className='course-background'>
-                <div className="course-text">Digital Skills</div>
-                <div className="course-overlay">
-                <div className="course-overlay-text">
-                        <p>module organiser: ---</p>
-                        <p>course start date: ---</p>
-                    </div>                
-                </div>
-                </div>
-            </div>
-
-            <div className="course-container">
-                <div className='course-background'>
-                <div className="course-text">Software Engineering</div>
-                <div className="course-overlay">
-                <div className="course-overlay-text">
-                        <p>module organiser: ---</p>
-                        <p>course start date: ---</p>
-                    </div>                
-                </div>
-                </div>
-            </div>
-            </div>
-
-            <div className="courserow2">
-            <div className="course-container">
-                <div className='course-background'>
-                <div className="course-text">Web Programming</div>
-                <div className="course-overlay">
-                <div className="course-overlay-text">
-                        <p>module organiser: ---</p>
-                        <p>course start date: ---</p>
+                <div className="courserow1">
+                    <div className="course-container">
+                        <div className='course-background'>
+                            <div className="course-text">EC5432 Digital Skills</div>
+                            <div className="course-overlay">
+                                <div className="course-overlay-text">
+                                    <p>Module organiser: Jane<p>
+                                    <p>Course start date: 22/09/2023</p></p></p>
+                                </div>                
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
-            </div>
 
-            <div className="course-container">
-                <div className='course-background'>
-                <div className="course-text">Algorithms</div>
-                <div className="course-overlay">
+                <div className="course-container">
+                    <div className='course-background'>
+                    <div className="course-text">EC6234 Software Engineering</div>
+                    <div className="course-overlay">
                     <div className="course-overlay-text">
-                        <p>module organiser: ---</p>
-                        <p>course start date: ---</p>
+                            <p>module organiser: ---</p>
+                            <p>course start date: ---</p>
+                        </div>                
+                    </div>
                     </div>
                 </div>
                 </div>
-            </div>
+
+                <div className="courserow2">
+                <div className="course-container">
+                    <div className='course-background'>
+                    <div className="course-text">EC6254 Web Programming</div>
+                    <div className="course-overlay">
+                    <div className="course-overlay-text">
+                            <p>module organiser: ---</p>
+                            <p>course start date: ---</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="course-container">
+                    <div className='course-background'>
+                    <div className="course-text">EC7634 Algorithms</div>
+                    <div className="course-overlay">
+                        <div className="course-overlay-text">
+                            <p>module organiser: ---</p>
+                            <p>course start date: ---</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
 
             {/* <div className="courserow1">
