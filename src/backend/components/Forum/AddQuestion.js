1485e69113b9870314bb5e4e953890998495c098
AdamBeji <91293290+AdamBeji@users.noreply.github.com>
2023-04-09 13:46:58 +0100

    Update AddQuestion.js

--- src/backend/components/Forum/AddQuestion.js
+++ src/backend/components/Forum/AddQuestion.js
@@ -1,27 +1,15 @@
-import React, {useState} from 'react';
 import { db } from '../../firebase';
-import { set, ref, push, update } from 'firebase/database';
+import { ref, push } from 'firebase/database';
 
-const AddQuestion=()=>{
-    const [question, setQuestion] = useState('');
-    
-    const addQuestion = (e) => {
-        e.preventDefault();
-        const questionRef = ref(db, 'questions');
-        const questionX = {
-            question
-        };
-        push(questionRef, questionX);
-      };
-  return (
-    <div className='add-course-container'>
-      <form onSubmit={addQuestion}>
-        <h1>Forum</h1>
-        <textarea placeholder='Ask a Question' value={question} onChange={(e) => setQuestion(e.target.value)}></textarea>
-        <button type='submit'>Submit Question</button>
-      </form>
-    </div>
-  );
+const AddQuestion=(question,courseID)=>{
+    const questionRef = ref(db, 'questions');
+    const questionX = {
+      question,
+      courseID
+    };
+    push(questionRef, questionX);
 };
 
+
+export { AddQuestion };
 export default AddQuestion;
