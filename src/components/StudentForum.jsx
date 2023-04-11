0c9eaadd72dd1a13241fef6518fffd2ebc70ac0b
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-04-11 14:56:19 +0100

    changes to forum design

--- src/components/StudentForum.jsx
+++ src/components/StudentForum.jsx
@@ -1,4 +1,6 @@
-import './StudentForum.css';
+// import './StudentForum.css';
+import '../backend/components/courseDatabase/ViewCourses'
+import '../backend/components/Forum/Forum.css'
 import React, { useState } from 'react';
 import AddQuestion from '../backend/components/Forum/AddQuestion';
 import ViewQuestions from '../backend/components/Forum/ViewQuestions';
@@ -32,21 +34,24 @@ const StudentForum = ({ signIn }) => {
           <div className='divstuff'>
             <img className='rs-logo' src='graphic6.gif' alt='graphic 6'></img>
           </div>
-          <h1 className='title'>Student Forum</h1>
-          <form onSubmit={handleSubmit}>
-            <input
+          
+          <form className = 'addCourse' onSubmit={handleSubmit}>
+            <h1 className='th'>Student Forum</h1>
+            <input className='inputfield'
               type='text'
               placeholder='Course ID'
               value={courseID}
               onChange={(event) => setcourseID(event.target.value)}
             />
-            <input
+            <br></br>
+            <input className='inputfield'
               type='text'
               placeholder='Ask a Question'
               value={question}
               onChange={(event) => setQuestion(event.target.value)}
             />
-            <div className='buttons'>
+            <br></br>
+            <div className = 'submitcoursebutton'>
               <button type='submit' id='post'>
                 Post
               </button>
