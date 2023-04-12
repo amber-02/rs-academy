89b3f124fcc47e67e94fe2a129ba7b2dd6420bb3
AdamBeji <91293290+AdamBeji@users.noreply.github.com>
2023-04-12 23:45:05 +0100

    Update MakeAnnouncement.js

--- src/backend/components/Forum/MakeAnnouncement.js
+++ src/backend/components/Forum/MakeAnnouncement.js
@@ -4,20 +4,20 @@ import { db } from '../../firebase';
 import { ref, push } from 'firebase/database';
 
 const MakeAn = () => {
-    const [announcement, setAnn] = useState('');
+    const [question, setAnn] = useState('');
     const [courseID, setID] = useState('');
 
     const addAnntoDatabase = (e) => {
         e.preventDefault();
         const annReference = ref(db, 'announcements');
-        const annToSubmit = { announcement, courseID };
+        const annToSubmit = { question, courseID };
         push(annReference, annToSubmit);
     };
 
   return (
     <form className = 'addCourse' onSubmit={addAnntoDatabase}>
         <h1>Add Announcement</h1>
-        <input className='inputfield' required type='text' placeholder='Announcement to make' value={announcement} onChange={(e) => setAnn(e.target.value)}></input>
+        <input className='inputfield' required type='text' placeholder='Announcement to make' value={question} onChange={(e) => setAnn(e.target.value)}></input>
         <br></br>
         <input className='inputfield' required type='text' placeholder='Course ID' value={courseID} onChange={(e) => setID(e.target.value)}></input>
         <br></br>
