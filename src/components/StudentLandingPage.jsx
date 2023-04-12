5ee696ecf408a36ff630e97a374278e1953c5424
amber-02 <91199607+amber-02@users.noreply.github.com>
2023-04-12 15:12:16 +0100

    SLP

--- src/components/StudentLandingPage.jsx
+++ src/components/StudentLandingPage.jsx
@@ -1,14 +1,23 @@
-// import './LandingPage.css';
+import './LandingPage.css';
 
 const StudentLandingPage = () => {
   return (
     <>
-      <div className='content'>
-        <div className = 'divstuff'>
-        <p>text</p>
+<div className='content-contact'>
+            <div className="head-items">
+              <div className='text'>
+                <h1 className ='mycourses'>My Courses</h1>
+                <div className='content-text-enrolled'>
+                  <div className='line'><h1 className='enrolled'>Enrolled on:</h1>
+                  </div>
+                  <p className = 'coursename'>coursename</p>
+                  <p className = 'coursename'>coursename</p>
+                </div>
+                <img className='LPgraphic' src='graphic8.gif' alt="graphic 8"></img>
+              </div>
+            </div>
+
         </div>
-          <p className='content-text'>Student Landing Page</p>
-      </div>
     </>
   )
 }
